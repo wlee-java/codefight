b3d541812af058d404db6627832be2470d86cd50
W Lee <wolee1977@gmail.com>
2018-02-22 11:51:03 -0500

    first push

---
+++ codefights/src/codefights/linkedlist/ListPalindrome.java
@@ -0,0 +1,26 @@
+package codefights.linkedlist;
+
+public class ListPalindrome {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	// Definition for singly-linked list:
+	 class ListNode<T> {
+	   ListNode(T x) {
+		     value = x;
+	   }
+	   T value;
+	   ListNode<T> next;
+	 }
+	
+	boolean isListPalindrome(ListNode<Integer> l) {
+
+		return false;
+	}
+	
+	
+
+	
+}
